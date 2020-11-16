6a4c50a8cb5a1522af0751b368fa2a1ab1f9ee1c
Swetha-Sundaran <74202408+Swetha-Sundaran@users.noreply.github.com>
2020-11-16 12:17:33 +0530

    Add files via upload

---
+++ LowerCase.java
@@ -0,0 +1,16 @@
+package day_3;
+
+import java.util.Scanner;
+
+public class LowerCase {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a string: ");
+		String str = in.nextLine();
+		System.out.println(str.toLowerCase());
+
+	}
+
+}
---
+++ Replace.java
@@ -0,0 +1,15 @@
+package day_3;
+
+import java.util.Scanner;
+
+public class Replace {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input a string: ");
+		String str = in.nextLine();
+		System.out.println(str.replace('d', 'h'));
+
+	}
+
+}
